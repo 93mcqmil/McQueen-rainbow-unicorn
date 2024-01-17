ab2bd55ea7430a87217ecbc6b770787c878f91b0
93mcqmil <mcqueen_1993@hotmail.com>
2024-01-17 13:10:35 +0100

    thirdcommit

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var animalList = ["apa", "orangutang", "gorilla", "get", "giraff"];
 console.log(animalList);
+var myObject = {
+    string: "McQueen",
+    age: 30,
+    boolean: true
+};
+console.log(myObject);
--- app.ts
+++ app.ts
@@ -1,3 +1,10 @@
 let animalList = ["apa", "orangutang", "gorilla", "get", "giraff"];
 console.log(animalList);
 
+let myObject = {
+
+    string: "McQueen",
+    age: 30,
+    boolean: true
+};
+console.log(myObject)
